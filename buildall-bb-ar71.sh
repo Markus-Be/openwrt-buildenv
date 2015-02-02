f8881dc175ea0a1d732d6a647327969cfb300928
n3ph <n3ph@terminal21.de>
2015-02-02 23:20:03 +0100

    cleanup

--- buildall-bb-ar71.sh
+++ buildall-bb-ar71.sh
@@ -7,8 +7,13 @@ MAKEOPTS="-j2"
 # fail on errors
 set +e
 
+# install build-env sources
 git clone $REMOTE openwrt
+
+# enter build-env
 cd openwrt
+
+# prepare package feeds
 cp feeds.conf.default feeds.conf
 echo "src-git luci2 http://git.openwrt.org/project/luci2/ui.git" >> feeds.conf
 echo "src-git cjdns git://github.com/seattlemeshnet/meshbox.git;for-14.07" >> feeds.conf
@@ -21,24 +26,9 @@ echo "src-git kadnode git://github.com/mwarning/KadNode.git" >> feeds.conf
 echo "src-git kadlibsodium git://github.com/mwarning/libsodium-openwrt.git" >> feeds.conf
 echo "src-git fswebcam git://github.com/fsphil/fswebcam.git" >> feeds.conf
 echo "src-git solarfestival git://github.com/freifunk/freifunk-leipzig/solarfestival-packages.git" >> feeds.conf
-#echo "src-git oldpackages http://git.openwrt.org/packages.git" >> feeds.conf
-
-scripts/feeds update -a
-
-# revert to batman-adv 2013.4.0
-# cd feeds/routing
-# git remote add github-routing git://github.com/openwrt-routing/packages.git
-# git fetch github-routing
-# git checkout -b batman-adv-backport
-# rm -r batman-adv
-# git checkout 89c2a8bb562412281d1ff070007be16d5a4d8f55 batman-adv
-# git commit -a -m "batman-adv: revert to 2013.4.0"
-# rm -r alfred
-# git checkout e2cfab7f287673b1d6854c59db6e710668d145f3 alfred
-# git commit -a -m "alfread: revert to 2013.4.0"
-# cd ../..
 
 # create index and install all packages
+scripts/feeds update -a
 scripts/feeds update -i
 scripts/feeds install -a
 
@@ -79,11 +69,13 @@ CONFIG_PACKAGE_LUCI=y
 CONFIG_PACKAGE_6IN4=y
 EOF
 
+# configure default config
 make defconfig
 
 # allow stuff to fail from here on
 set -e
 
+# paste commands
 echo "dont forget to set batman-adv devel version git commit number manually if you need that"
 echo "please do it within screen###"
 echo "# make everything with"
